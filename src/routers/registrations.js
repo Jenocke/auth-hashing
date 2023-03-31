dfd191be1b92caa4e0af7d88020eeeb5ca8deff5
Jenocke <jenocke@gmail.com>
2023-03-31 13:47:08 +0200

    done

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
+const { user } = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
+router.post("/", async (req, res) => {
+  // Get the username and password from request body
+  const { username, password } = req.body;
+  const saltRounds = 5;
+
+  // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+  bcrypt.hash(password, saltRounds).then(async function (hash) {
     // Save the user using the prisma user model, setting their password to the hashed version
-    
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        password: hash,
+      },
+    });
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res.status(201).json({ user: newUser });
+  });
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,22 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    // Get the username and password from the request body
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  // Get the username and password from the request body
 
-    // Check that a user with that username exists in the database
-    // Use bcrypt to check that the provided password matches the hashed password on the user
-    // If either of these checks fail, respond with a 401 "Invalid username or password" error
-
+  // Check that a user with that username exists in the database
+  // Use bcrypt to check that the provided password matches the hashed password on the user
+  // If either of these checks fail, respond with a 401 "Invalid username or password" error
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+  if (user) {
+    const match = await bcrypt.compare(password, user.password);
     // If the user exists and the passwords match, create a JWT containing the username in the payload
     // Use the JWT_SECRET environment variable for the secret key
+    if (match) {
+      const payload = { username };
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+      res.status(200).json({ token });
+    }
 
     // Send a JSON object with a "token" key back to the client, the value is the JWT created
+  }
 });
 
 module.exports = router;
